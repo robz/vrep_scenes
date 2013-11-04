13e81b1f7cf6dcdfc59ebd56d8dccb20cec6d1eb
Robby <geomasta21@aol.com>
2013-11-04 06:19:48 -0600

    now setting the speed of the plant motor and reading joint positions to tell when to stop

--- remote api test/remote_control.py
+++ remote api test/remote_control.py
@@ -9,11 +9,21 @@ clientID = vrep.simxStart('127.0.0.1',6666,True,True,5000,5)
     
 print "hello vrep! ", clientID
 
+print "starting the motor..."
+
 error,jointHandle = vrep.simxGetObjectHandle(clientID, "plantStandMotor", vrep.simx_opmode_oneshot_wait)
+vrep.simxSetJointTargetVelocity(clientID, jointHandle, 1.0, vrep.simx_opmode_oneshot_wait);
+
+print "getting joint positions..."
+
 vrep.simxGetJointPosition(clientID, jointHandle, vrep.simx_opmode_streaming);
 
+started = False
+
+print "spinning 360 degrees..."
+
 # The control loop:
-while vrep.simxGetConnectionId(clientID)!=-1 : # while we are connected to the server..
+while vrep.simxGetConnectionId(clientID) != -1 : # while we are connected to the server..
     (error,position) = vrep.simxGetJointPosition(
         clientID,
         jointHandle,
@@ -23,7 +33,17 @@ while vrep.simxGetConnectionId(clientID)!=-1 : # while we are connected to the s
     # Fetch the newest joint value from the inbox (func. returns immediately (non-blocking)):
     if error==vrep.simx_error_noerror : 
         # here we have the newest joint position in variable jointPosition! 
-        print position
+        # break when we've done a 360
+        if started and position >= 0 and position < 1:
+            break
+        elif not started and position > 1:
+            started = True
+
+print "stoppping..."
+        
+vrep.simxSetJointTargetVelocity(clientID, jointHandle, 0.0, vrep.simx_opmode_oneshot_wait);
 
 if clientID != -1:
     vrep.simxFinish(clientID)
+
+print "done!"
